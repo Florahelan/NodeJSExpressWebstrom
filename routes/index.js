25d7a5041572baaf9f13ac9e1cb04b8b5d86a5c8
florahelan <florahelanh@gmail.com>
2017-10-25 23:21:44 -0700

    Adding db pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var app= router;
+var pg = require('pg');
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to CS 6320 ' });
+    res.render('index', { title: 'Express' });
+});
+module.exports = router;
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err) {
+                console.error(err); response.send("Error " + err);
+            }
+            else {
+                response.render('/pages/db', {results: result.rows} );
+            }
+        });
+    });
 });
 
-module.exports = router;
+
+
+
